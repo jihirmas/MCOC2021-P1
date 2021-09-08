6809e6730d3120a0d2fc087aca2fee4fc86eebdb
TomasSchilling <69157271+TomasSchilling@users.noreply.github.com>
2021-09-08 20:26:49 -0300

    Add files via upload

--- reticulado.py
+++ reticulado.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 from scipy.linalg import solve
 
@@ -7,35 +8,41 @@ class Reticulado(object):
 
     def __init__(self):
         super(Reticulado, self).__init__()
+        self.xyz = np.zeros((Reticulado.__NNodosInit__,3), dtype=np.double)
+        self.Nnodos = 0
+        self.barras = []
+        self.Ndimensiones = 2
         
-        
-        """Implementar"""	
-        
-
 
     def agregar_nodo(self, x, y, z=0):
-        
-        """Implementar"""	
-        
-        return 0
+        if self.Nnodos+1 > Reticulado.__NNodosInit__:
+            self.xyz.resize((self.Nnodos+1,3))
+        self.xyz[self.Nnodos,:] = [x, y, z]
+        self.Nnodos += 1
+        if z != 0.:
+            self.Ndimensiones = 3
+            
+
+
 
     def agregar_barra(self, barra):
+        self.barras.append(barra)
         
-        """Implementar"""	
-        
-        return 0
+
 
     def obtener_coordenada_nodal(self, n):
         
-        """Implementar"""	
-        
-        return 0
+        return self.xyz[n,:]
+
 
     def calcular_peso_total(self):
         
-        """Implementar"""	
-        
-        return 0
+        peso_total=0
+        for i in barras:
+            peso_total+= i.calcular_peso(self)
+        return peso_total
+
+
 
     def obtener_nodos(self):
         
